261081ca75847c873d7aa06a7acd9798204f22ec
consta_n <consta_n@localhost.localdomain>
2015-05-02 21:52:50 +0200

    add_first

--- add_node.c
+++ add_node.c
@@ -5,7 +5,7 @@
 ** Login   <consta_n@epitech.net>
 ** 
 ** Started on  Sat May  2 21:18:48 2015 consta_n
-** Last update Sat May  2 21:27:44 2015 consta_n
+** Last update Sat May  2 21:52:33 2015 consta_n
 */
 
 #include "lem_in.h"
@@ -26,13 +26,18 @@ int		add_ant(t_ant **ant, char *name)
 int		add_weight(t_weight **weight, char *name)
 {
   t_weight	*node;
+  static int	a = 0;
 
   if ((node = malloc(sizeof(t_weight))) == NULL)
     return (my_error("struct.c : 60 Malloc Failed", -1));
   node->name = name;
-  node->weight = -1;
+  if (a)
+    node->weight = -1;
+  else
+    node->weight = 0;
   node->parcour = 0;
   node->next = *weight;
   *weight = node;
+  a = 1;
   return (0);
 }
